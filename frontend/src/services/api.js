42e20d470b49fc5d29b722bd325b998015fe1a5a
Ayush Kumar <akofficial1330@gmail.com>
2025-06-04 20:17:13 +0530

    Initial commit: Add backend and frontend code

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api/resumes", // Base path for all resume endpoints
+});
+
+export const uploadResume = (formData) =>
+  api.post("/upload", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+export const getResumes = () => api.get("/");
+export const deleteResume = (id) => api.delete(`/${id}`);
